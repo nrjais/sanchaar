401c5b17a8ba3fd307523c66d9a6e6846acce7c4
Neeraj Jaiswal <neerajj85@gmail.com>
2023-12-29 16:32:47 +0530

    Fix deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "node:url";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [
     vue(),
+    VueDevTools(),
     AutoImport({
       imports: [
         "vue",
