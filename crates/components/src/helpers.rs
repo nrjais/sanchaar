bd3b2341ebe9edd21a77eaad2dca6734f3fa9c6b
Neeraj Jaiswal <neerajj85@gmail.com>
2024-05-06 14:08:23 +0530

    Fix tooltip and design

--- crates/components/src/helpers.rs
+++ crates/components/src/helpers.rs
@@ -1,6 +1,6 @@
 use iced::{
     widget::{container, text, tooltip::Position, Container, Tooltip},
-    Element, Length,
+    Border, Element, Length, Theme,
 };
 
 pub fn expanded<'a, M>(base: impl Into<Element<'a, M>>) -> Container<'a, M> {
@@ -9,10 +9,22 @@ pub fn expanded<'a, M>(base: impl Into<Element<'a, M>>) -> Container<'a, M> {
 
 pub fn tooltip<'a, M: 'a>(msg: &'static str, base: impl Into<Element<'a, M>>) -> Tooltip<'a, M> {
     iced::widget::tooltip(
-        container(text(msg))
-            .padding([2, 4])
-            .style(container::bordered_box),
         base,
+        container(text(msg))
+            .style(|theme: &Theme| {
+                let palette = theme.extended_palette();
+
+                container::Style {
+                    background: Some(palette.background.weak.color.into()),
+                    border: Border {
+                        width: 1.0,
+                        radius: 4.0.into(),
+                        color: palette.background.strong.color,
+                    },
+                    ..Default::default()
+                }
+            })
+            .padding([2, 4]),
         Position::Bottom,
     )
 }
