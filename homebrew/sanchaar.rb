e15fb379a929da7a266fc7ffa33d465641075cf5
Neeraj Jaiswal <neerajj85@gmail.com>
2025-10-05 16:55:38 +0530

    add brew checksum

--- homebrew/sanchaar.rb
+++ homebrew/sanchaar.rb
@@ -1,11 +1,12 @@
 cask "sanchaar" do
   version "0.1.2"
-  sha256 :no_check # Will be updated during release
 
   on_arm do
+    sha256 "REPLACE_WITH_ARM64_SHA256"
     url "https://github.com/nrjais/sanchaar/releases/download/#{version}/Sanchaar-#{version}-aarch64-apple-darwin.dmg"
   end
   on_intel do
+    sha256 "REPLACE_WITH_X86_64_SHA256"
     url "https://github.com/nrjais/sanchaar/releases/download/#{version}/Sanchaar-#{version}-x86_64-apple-darwin.dmg"
   end
 
@@ -14,7 +15,7 @@ cask "sanchaar" do
   homepage "https://github.com/nrjais/sanchaar"
 
   livecheck do
-    url :url
+    url :homepage
     strategy :github_latest
   end
 
